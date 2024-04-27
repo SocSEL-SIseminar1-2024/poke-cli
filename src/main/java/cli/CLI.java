fb6e0ad00abf5e4dc780d9a69506efa593dd5eac
MYano0960 <142595574+MYano0960@users.noreply.github.com>
2024-04-28 00:04:30 +0900

    ポケモンの名前を繰り返し出力するプログラムの修正
    getコマンドかstatusコマンドで繰り返し入力できるようにした際にfinishが入力されたときのみ繰り返しの中断処理が実行されるように修正しました。

--- src/main/java/cli/CLI.java
+++ src/main/java/cli/CLI.java
@@ -1,11 +1,13 @@
 package cli;
 
+import java.util.Scanner;
 import cli.commands.get.GetPokeNameList;
 import cli.commands.status.GetPokeStatus;
 import cli.commands.hello.HelloCommand;
 
 public class CLI implements Runnable {
   private String[] args;
+  Scanner sc = new Scanner(System.in);
 
   CLI(String[] args) {
     this.args = args;
@@ -16,28 +18,60 @@ public class CLI implements Runnable {
     try {
       // コマンドを格納(poke get なら get)
       String command = args[0];
-      // オプション格納(poke get 10なら 10)
-      String option = null;
-
-      // オプションが存在する時だけ変数に入れる
-      if (args.length == 2) {
-        option = args[1];
-      }
 
       // コマンドごとに処理を分岐
-      if (option != null && command.equals("get")) {
-        int limit = Integer.parseInt(option);
-        new GetPokeNameList(limit).run();
-      }
-      
-      if (option != null && command.equals("status")) {
-        String name = option;
-        new GetPokeStatus(name).run();
-      }
-      
-      if (command.equals("hello")) {
+      if (command.equals("get")) {
+        /* getコマンドが実行されると整数値を繰り返し入力できるようになる。
+        入力値にfinishを入力すると繰り返し処理が終了する。*/
+        while (true){
+          System.out.print("PokeNumber: ");
+          if (sc.hasNextInt()){
+            String InputNum = sc.next();
+            int GetPokeNum = Integer.parseInt(InputNum);
+            if (GetPokeNum > 0 && GetPokeNum <= 1302){
+              new GetPokeNameList(GetPokeNum).run();
+            } else {
+              System.out.println("Not exist");
+            }
+          } else {
+            if (sc.hasNext("finish")){
+              System.out.println("See you again!");
+              break;
+            } else {
+              System.out.println("Input Error!");
+              sc.next();
+              continue;
+            }
+          }
+        }
+      } else if (command.equals("status")){
+        while (true){
+          /*  statusコマンドも同様に実行されると整数値を繰り返し入力できるようになる。
+        入力値にfinishを入力すると繰り返し処理が終了する。*/
+          System.out.print("PokeNumber: ");
+          if (sc.hasNextInt()){
+            String InputNum = sc.next();
+            int GetPokeNum = Integer.parseInt(InputNum);
+            if (GetPokeNum > 0 && GetPokeNum <= 1302){
+              new GetPokeStatus(GetPokeNum).run();
+            } else {
+              System.out.println("Not exist");
+            }
+          } else {
+            if (sc.hasNext("finish")){
+              System.out.println("See you again!");
+              break;
+            } else {
+              System.out.println("Input Error!");
+              sc.next();
+              continue;
+            }
+          }
+        }
+      } else if (command.equals("hello")) {
         new HelloCommand().run();
       }
+      sc.close();
     } catch (Exception e) {
       e.printStackTrace();
     }
